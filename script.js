833581b272fc632b5281561cb3ae1dc2e64e9291
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-09-25 22:48:44 +0530

    clickme btn

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ function animationTime(){
 }
 
 setInterval(animationTime, totalTime)
+
+const colors = ['1','2','3','4']
+let random = {}
+let clickMe = document.getElementById("btn")
+const color = document.querySelector('.color')
+
+clickMe.addEventListener('click', function(){
+  const randomNumber = randomColor() 
+  console.log(randomNumber)
+  document.body.style.background = `url('./img/bg${colors[randomNumber]}.jpg')`
+  color.className = `gradient-circle${colors[randomNumber]}`
+})
+
+function randomColor(){
+  return Math.floor(Math.random()*colors.length)  
+}
